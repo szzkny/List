21878cd77abaf82d0407e5ab346018391a4f603d
Molnár-Zékány Szabina <szabina.zekany@bayzoltan.hu>
2023-03-28 14:58:00 +0200

    Upload

---
+++ List/Equipment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    internal class Equipment
+    {
+        public string Owner { get; set; }
+        public string EquipmentName{ get; set; }
+
+        public override string ToString()
+        {
+            return Owner.ToUpper() + " has a " + EquipmentName.ToUpper() + " in his/her bag!";
+        }
+    }
+}
---
+++ List/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Equipment> list = new List<Equipment>();
+
+            Console.WriteLine("Capacity of the list: " + list.Capacity);
+            Console.WriteLine("Length of the list: " + list.Count + "\n");
+
+            list.Add(new Equipment() { Owner = "John", EquipmentName = "headlamp" });
+            list.Add(new Equipment() { Owner = "Mary", EquipmentName = "headlamp" });
+            list.Add(new Equipment() { Owner = "Joe", EquipmentName = "socks" });
+            list.Add(new Equipment() { Owner = "Joe", EquipmentName = "blanket" });
+            list.Add(new Equipment() { Owner = "Chris", EquipmentName = "raincoat" });
+            list.Add(new Equipment() { Owner = "Mary", EquipmentName = "knife" });
+            //list.Add(new Equipment() { Owner = "Ellen", EquipmentName = "knife" });
+
+            Console.WriteLine("Capacity of the list: " + list.Capacity);
+            Console.WriteLine("Length of the list: " + list.Count + "\n");
+
+            list.TrimExcess();
+
+            Console.WriteLine("Capacity of the list: " + list.Capacity);
+            Console.WriteLine("Length of the list: " + list.Count + "\n");
+
+            list.RemoveAt(3);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine();
+
+            Equipment equipment = new Equipment();
+            equipment.Owner = "Sam";
+            equipment.EquipmentName = "gourd";
+
+            list.Insert(2, equipment);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine();
+
+            list.Remove(equipment);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine();
+
+            list.Clear();
+            //list.Capacity = 0;
+            list.TrimExcess();
+
+            Console.WriteLine("Capacity of the list: " + list.Capacity);
+            Console.WriteLine("Length of the list: " + list.Count + "\n");
+        }
+    }
+}
